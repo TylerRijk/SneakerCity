5a91bb852118db50cbafa55d5a7508c7e1808b9a
TylerRijk <tyler.derijk@student.hu.nl>
2023-06-16 14:11:20 +0200

    SneakerResource.java verbeterd a.d.h.v feedback Sprint 2 (checks toegevoegd)

--- src/main/java/webservices/SneakerResource.java
+++ src/main/java/webservices/SneakerResource.java
@@ -9,6 +9,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Map;
 
 @Path("/sneakers")
 public class SneakerResource {
@@ -16,7 +17,12 @@ public class SneakerResource {
     @Produces(MediaType.APPLICATION_JSON)
     public Response getAllSneakers() {
         ArrayList<Sneaker> sneakers = PersistenceSneaker.loadAllSneakers();
-        return Response.status(Response.Status.OK).entity(sneakers).build();
+
+        if (sneakers.isEmpty()) {
+            return Response.status(Response.Status.NOT_FOUND).entity("Geen sneakers gevonden").build();
+        } else {
+            return Response.status(Response.Status.OK).entity(sneakers).build();
+        }
     }
 
     @GET
@@ -34,6 +40,25 @@ public class SneakerResource {
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     public Response createSneaker(SneakerRequest sneakerRequest) {
+        if (sneakerRequest.artikelnummer == 0
+                || sneakerRequest.merk == null || sneakerRequest.merk.isEmpty()
+                || sneakerRequest.kleur == null || sneakerRequest.kleur.isEmpty()
+                || sneakerRequest.maat == 0
+                || sneakerRequest.beschrijving == null || sneakerRequest.beschrijving.isEmpty()
+                || sneakerRequest.prijs == 0
+                || sneakerRequest.image == null || sneakerRequest.image.isEmpty()
+                || sneakerRequest.voorraad == 0 ) {
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+
+        ArrayList<Sneaker> sneakers = PersistenceSneaker.loadAllSneakers();
+
+        for (Sneaker sneaker : sneakers) {
+            if (sneaker.getArtikelnummer() == sneakerRequest.artikelnummer) {
+                return Response.status(Response.Status.CONFLICT).build();
+            }
+        }
+
         PersistenceSneaker.saveSneaker(new Sneaker(sneakerRequest.artikelnummer, sneakerRequest.merk, sneakerRequest.kleur, sneakerRequest.maat, sneakerRequest.beschrijving, sneakerRequest.prijs, sneakerRequest.image, sneakerRequest.voorraad));
         return Response.status(Response.Status.CREATED).build();
     }
